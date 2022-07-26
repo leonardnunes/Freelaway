5d9880af4647166113bb19b1f8ed85224d40ee75
Leonardo Nunes <leonardo21.nunes@gmail.com>
2022-07-26 16:30:11 -0300

    Arquivos do Heroku

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m$v@rxpo@h*-^qi*4iq#(&^od+#&bj@lsnaokjr!voyhyyw-3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
