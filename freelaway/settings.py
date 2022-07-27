7e77f73530cdf079de28194e36dd85b5273c668e
Leonardo Nunes <leonardo21.nunes@gmail.com>
2022-07-27 18:37:18 -0300

    Projeto finalizado

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 
@@ -26,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m$v@rxpo@h*-^qi*4iq#(&^od+#&bj@lsnaokjr!voyhyyw-3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False  # https://freelaway-free.herokuapp.com/
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://freelaway-free.herokuapp.com/']
 
 # Application definition
 
@@ -121,6 +122,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
